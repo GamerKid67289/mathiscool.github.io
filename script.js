2a315a520d672bfbf29b02dabdb9ea896f44fde2
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-11-02 17:17:22 -0400

    e

--- script.js
+++ script.js
@@ -1,36 +1,22 @@
 function search() {
   var searchQuery = document.getElementById("search-input").value;
+  // Here you can perform a search using the searchQuery.
+  // You can fetch data from an API or perform a search on a predefined dataset.
 
-  // Call a function to retrieve search results from the database
-  var results = getSearchResults(searchQuery);
-
-  // Display the results on the page
-  displayResults(results);
-}
-
-function getSearchResults(searchQuery) {
-  // Here, you can implement the logic to query your custom database and retrieve the relevant search results based on the searchQuery
-  // This may involve connecting to the database using a server-side language like Python or Node.js and executing a query
-
-  // For simplicity, let's just return a static list of results
-  var staticResults = [
-    { title: "1v1.lol", url: "https://www.1v1.lol" },
-    { title: "Example Result 2", url: "#" },
-    { title: "Example Result 3", url: "#" }
+  // For simplicity, let's just display some static search results.
+  var searchResults = [
+      "Result 1",
+      "Result 2",
+      "Result 3",
+      "Result 4"
   ];
 
-  return staticResults;
-}
+  var resultsContainer = document.getElementById("search-results");
+  resultsContainer.innerHTML = ""; // Clear previous results
 
-function displayResults(results) {
-  var searchResultsContainer = document.getElementById("search-results");
-  searchResultsContainer.innerHTML = "";
-
-  results.forEach(function(result) {
-    var link = document.createElement("a");
-    link.href = result.url;
-    link.textContent = result.title;
-    searchResultsContainer.appendChild(link);
-    searchResultsContainer.appendChild(document.createElement("br"));
+  searchResults.forEach(function(result) {
+      var resultElement = document.createElement("p");
+      resultElement.textContent = result;
+      resultsContainer.appendChild(resultElement);
   });
 }
