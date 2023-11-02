aa41a27ffbe495d510f4d6e7714952416b1133bb
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-11-02 17:09:43 -0400

    e

--- script.js
+++ script.js
@@ -1,43 +1,36 @@
-// Database of relevant searches
-const database = [
-  {
-    title: "Full Stack Development",
-    content: "Learn the skills necessary to become a full stack developer."
-  },
-  {
-    title: "HTML",
-    content: "Hypertext Markup Language, the standard markup language for documents designed to be displayed in a web browser."
-  },
-  {
-    title: "CSS",
-    content: "Cascading Style Sheets, a style sheet language used for describing the look and formatting of a document written in HTML."
-  },
-  {
-    title: "JavaScript",
-    content: "A high-level, interpreted programming language that enables interactive web pages and makes them dynamic."
-  },
-  // Add more relevant searches to the database as needed
-];
-
 function search() {
-  const searchTerm = document.getElementById("searchInput").value.toLowerCase();
-  const resultsDiv = document.getElementById("results");
-  resultsDiv.innerHTML = "";
+  var searchQuery = document.getElementById("search-input").value;
 
-  // Loop through the database and find matches
-  for (let i = 0; i < database.length; i++) {
-    const searchTitle = database[i].title.toLowerCase();
-    const searchContent = database[i].content.toLowerCase();
+  // Call a function to retrieve search results from the database
+  var results = getSearchResults(searchQuery);
 
-    if (searchTitle.includes(searchTerm) || searchContent.includes(searchTerm)) {
-      const resultDiv = document.createElement("div");
-      resultDiv.className = "searchResult";
-      resultDiv.innerHTML = `<h3>${database[i].title}</h3><p>${database[i].content}</p>`;
-      resultsDiv.appendChild(resultDiv);
-    }
-  }
+  // Display the results on the page
+  displayResults(results);
+}
 
-  if (resultsDiv.innerHTML === "") {
-    resultsDiv.innerHTML = "No results found.";
-  }
+function getSearchResults(searchQuery) {
+  // Here, you can implement the logic to query your custom database and retrieve the relevant search results based on the searchQuery
+  // This may involve connecting to the database using a server-side language like Python or Node.js and executing a query
+
+  // For simplicity, let's just return a static list of results
+  var staticResults = [
+    { title: "1v1.lol", url: "https://www.1v1.lol" },
+    { title: "Example Result 2", url: "#" },
+    { title: "Example Result 3", url: "#" }
+  ];
+
+  return staticResults;
+}
+
+function displayResults(results) {
+  var searchResultsContainer = document.getElementById("search-results");
+  searchResultsContainer.innerHTML = "";
+
+  results.forEach(function(result) {
+    var link = document.createElement("a");
+    link.href = result.url;
+    link.textContent = result.title;
+    searchResultsContainer.appendChild(link);
+    searchResultsContainer.appendChild(document.createElement("br"));
+  });
 }
