f38e432c814af95cdea5770ec52a5618f815d27a
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-11-02 17:07:00 -0400

    e

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-// Sample database with search results
+// Database of relevant searches
 const database = [
-    { title: "1v1 lol", url: "https://1v1.lol" },
-    { title: "Tutorial 2", url: "https://example.com/tutorial-2" },
-    { title: "Blog Post 1", url: "https://example.com/blog-1" },
-    { title: "Blog Post 2", url: "https://example.com/blog-2" }
-  ];
-  
-  function search() {
-    // Clear previous results
-    document.getElementById("searchResults").innerHTML = "";
-  
-    // Get search query
-    const query = document.getElementById("searchInput").value.toLowerCase();
-  
-    // Search database for matching titles
-    const results = database.filter(item => item.title.toLowerCase().includes(query));
-  
-    // Display search results
-    results.forEach(item => {
-      const link = document.createElement("a");
-      link.href = item.url;
-      link.textContent = item.title;
-      link.target = "_blank"; // Opens link in a new tab/window
-      document.getElementById("searchResults").appendChild(link);
-      document.getElementById("searchResults").appendChild(document.createElement("br"));
-    });
+  {
+    title: "Full Stack Development",
+    content: "Learn the skills necessary to become a full stack developer."
+  },
+  {
+    title: "HTML",
+    content: "Hypertext Markup Language, the standard markup language for documents designed to be displayed in a web browser."
+  },
+  {
+    title: "CSS",
+    content: "Cascading Style Sheets, a style sheet language used for describing the look and formatting of a document written in HTML."
+  },
+  {
+    title: "JavaScript",
+    content: "A high-level, interpreted programming language that enables interactive web pages and makes them dynamic."
+  },
+  // Add more relevant searches to the database as needed
+];
+
+function search() {
+  const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+  const resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = "";
+
+  // Loop through the database and find matches
+  for (let i = 0; i < database.length; i++) {
+    const searchTitle = database[i].title.toLowerCase();
+    const searchContent = database[i].content.toLowerCase();
+
+    if (searchTitle.includes(searchTerm) || searchContent.includes(searchTerm)) {
+      const resultDiv = document.createElement("div");
+      resultDiv.className = "searchResult";
+      resultDiv.innerHTML = `<h3>${database[i].title}</h3><p>${database[i].content}</p>`;
+      resultsDiv.appendChild(resultDiv);
+    }
   }
-  
+
+  if (resultsDiv.innerHTML === "") {
+    resultsDiv.innerHTML = "No results found.";
+  }
+}
