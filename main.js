46ed4c625b2d1026dd9d7c92e8962cebd11e6d12
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-11-02 17:02:31 -0400

    e

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
-// Assuming you have a 'database' array of objects with 'title' and 'content' properties
+// Sample database with search results
 const database = [
-    { title: 'First Post', content: 'This is the content of the first post.' },
-    { title: 'Second Post', content: 'Here is the content of the second post.' },
-    { title: 'Third Post', content: 'Content of the third post goes here.' }
-];
-
-const searchForm = document.getElementById('searchForm');
-const searchInput = document.getElementById('searchInput');
-const searchResults = document.getElementById('searchResults');
-
-searchForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
-
-    const searchTerm = searchInput.value.toLowerCase();
-    const matchingResults = database.filter(function(post) {
-        return post.title.toLowerCase().includes(searchTerm) || post.content.toLowerCase().includes(searchTerm);
+    { title: "1v1 lol", url: "https://1v1.lol" },
+    { title: "Tutorial 2", url: "https://example.com/tutorial-2" },
+    { title: "Blog Post 1", url: "https://example.com/blog-1" },
+    { title: "Blog Post 2", url: "https://example.com/blog-2" }
+  ];
+  
+  function search() {
+    // Clear previous results
+    document.getElementById("searchResults").innerHTML = "";
+  
+    // Get search query
+    const query = document.getElementById("searchInput").value.toLowerCase();
+  
+    // Search database for matching titles
+    const results = database.filter(item => item.title.toLowerCase().includes(query));
+  
+    // Display search results
+    results.forEach(item => {
+      const link = document.createElement("a");
+      link.href = item.url;
+      link.textContent = item.title;
+      link.target = "_blank"; // Opens link in a new tab/window
+      document.getElementById("searchResults").appendChild(link);
+      document.getElementById("searchResults").appendChild(document.createElement("br"));
     });
-
-    displayResults(matchingResults);
-});
-
-function displayResults(results) {
-    searchResults.innerHTML = ''; // Clear previous results
-
-    if (results.length === 0) {
-        searchResults.innerHTML = 'No results found.';
-        return;
-    }
-
-    results.forEach(function(post) {
-        const postElement = document.createElement('div');
-        postElement.innerHTML = `
-            <h3>${post.title}</h3>
-            <p>${post.content}</p>
-        `;
-        searchResults.appendChild(postElement);
-    });
-}
+  }
+  
